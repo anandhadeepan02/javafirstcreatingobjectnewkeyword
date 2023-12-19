3ad252c15e1202a9433a8e7e8fb8e29989a95aef
anand <anand@Anandhadeepan>
2023-12-19 19:08:11 +0530

    nochanges

---
+++ src/java1/First.java
@@ -0,0 +1,18 @@
+package java1;
+
+public class First {
+	String s = "testing";
+	 int p = 234;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int m=100;
+System.out.println("The value of local variable is:" +m);
+//creating a object using new keyword
+First a =new First();
+System.out.println(a.s);
+System.out.println(a.p);
+
+	}
+
+}
